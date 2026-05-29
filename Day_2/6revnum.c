aecae10e0b5d7b71f056b2fcfd09899f947aeecd
akshatjain250106-coder <akshatjain250106@gmail.com>
2026-05-29 19:31:50 +0530

    Add 6revnum file

---
+++ Day_2/6revnum.c
@@ -0,0 +1,14 @@
+//reverse a number
+#include<stdio.h>
+int main(){
+    int n,d,rev=0;
+    printf("enter a number\n");
+    scanf("%d",&n);
+    while(n>0){
+        d=n%10;
+        rev=rev*10+d;
+        n=n/10;
+    }
+    printf("number after reversing is %d",rev);
+    return 0;
+}
